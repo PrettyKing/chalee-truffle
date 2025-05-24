ae0a64d655991dccf85f373fad8b94fa0af7ed4f
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-05-25 00:24:45 +0800

    更新部署脚本，添加PayableDemo合约部署

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const InfoContract = artifacts.require('InfoContract');
+const PayableDemo = artifacts.require('PayableDemo');
 
 module.exports = function (deployer) {
   deployer.deploy(InfoContract);
+  deployer.deploy(PayableDemo);
 };
