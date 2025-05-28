619c83d00d4809681b162d2b70258ae2e4221551
chalee <695469701@qq.com>
2025-05-28 20:17:24 +0800

    feat: update red packet

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,9 @@
 const InfoContract = artifacts.require('InfoContract');
 const PayableDemo = artifacts.require('PayableDemo');
+const RedPacket = artifacts.require('RedPacket');
 
 module.exports = function (deployer) {
   deployer.deploy(InfoContract);
   deployer.deploy(PayableDemo);
+  deployer.deploy(RedPacket);
 };
