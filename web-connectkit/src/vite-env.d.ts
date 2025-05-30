cc7244ee79119922d87b869772a1ba71d6dc7c89
chalee <695469701@qq.com>
2025-05-31 00:27:41 +0800

    fix: U

--- web-connectkit/src/vite-env.d.ts
+++ web-connectkit/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_REACT_APP_PROJECT_ID: string
+    readonly VITE_REACT_APP_PROJECT_ID: string
+    readonly VITE_REACT_APP_CONTRACT_ADDRESS: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
