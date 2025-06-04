3e6c9bfc069ac15b1e613d9a6354923d42083c0a
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-06-04 13:35:10 +0800

    Simplify WalletConnect component

--- web-connectkit/src/components/WalletConnect.jsx
+++ web-connectkit/src/components/WalletConnect.jsx
@@ -2,7 +2,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function WalletConnect() {
   return (
-    <div className="flex justify-center mb-8">
+    <div className="flex justify-center mb-6">
       <ConnectButton />
     </div>
   );
