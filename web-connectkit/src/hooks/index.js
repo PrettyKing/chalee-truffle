0737f92e426fd31ce26cbe34c9b61699cbbbcd71
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-06-05 13:57:42 +0800

    创建 hooks 的 index 文件

---
+++ web-connectkit/src/hooks/index.js
@@ -0,0 +1,11 @@
+// 主要的 Hook - 组合了所有功能
+export { useRedPacket } from './useRedPacket';
+
+// 专门的 Hooks - 可以单独使用
+export { useContract } from './useContract';
+export { useRedPacketQuery } from './useRedPacketQuery';
+export { useRedPacketCreate } from './useRedPacketCreate';
+export { useRedPacketGrab } from './useRedPacketGrab';
+export { useContractBalance } from './useContractBalance';
+export { useUserInfo } from './useUserInfo';
+export { useOwnerOperations } from './useOwnerOperations';
