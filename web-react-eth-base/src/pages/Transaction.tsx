ac7bcd5afa7a6d0aa31d770797d5aed162cf1e57
chalee <695469701@qq.com>
2025-06-11 21:00:52 +0800

    fix: u

--- web-react-eth-base/src/pages/Transaction.tsx
+++ web-react-eth-base/src/pages/Transaction.tsx
@@ -12,7 +12,7 @@ export const Transaction: React.FC = () => {
 function SendTransaction() {
   const { address } = useAccount();
   const { data: balance } = useBalance({ address });
-  const { sendTransaction } = useSendTransaction();
+  const { sendTransactionAsync } = useSendTransaction();
 
   const onsubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,18 +22,15 @@ function SendTransaction() {
     const data = formData.get('data') as `0x${string}` | undefined;
     const parsedAmount = parseEther(amount);
     try {
-      const tx = sendTransaction({
+      const tx = await sendTransactionAsync({
         to: recipient,
         value: parsedAmount,
         ...(data ? { data: stringToHex(data) } : {}),
       });
       console.log('Transaction sent:', tx);
-      // 等待交易确认
-      // const receipt = await useWaitForTransactionReceipt({ hash: tx.hash });
-      // if (receipt) {
-      //   console.log('Transaction confirmed:', receipt);
-      //   alert('交易已成功发送！');
-      // }
+      // setTransitionHex(tx);
+      console.log('Transaction receipt:', data);
+
     } catch (error) {
       console.error('Transaction failed:', error);
       alert('交易失败，请检查输入信息。');
@@ -103,5 +100,5 @@ function stringToHex(str: string): `0x${string}` {
   const hex = Array.from(str)
     .map(char => char.charCodeAt(0).toString(16).padStart(2, '0'))
     .join('');
-  return '0x' + hex as `0x${string}`; // 确保返回类型符合 `0x` 前缀的十六进制字符串格式
+  return ('0x' + hex) as `0x${string}`; // 确保返回类型符合 `0x` 前缀的十六进制字符串格式
 }
